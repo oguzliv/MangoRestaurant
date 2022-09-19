96f6338ca56bde78af4b077fe7736f2b08956bee
oguzliv <oguzliv@gmail.com>
2022-09-19 18:10:48 +0300

    rabbitmq ctd

---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,12 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public class RabbitMQCartMessageSender : IRabbitMQMessageSender
+    {
+        public void SendMessage(BaseMessage baseMessage, string queueName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
